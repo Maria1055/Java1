4344a1910c64be2b5ad050fc25171b9ba578afaf
Maria1055 <96591820+Maria1055@users.noreply.github.com>
2022-03-04 02:16:43 +0700

    git commit

--- src/Main1.java
+++ src/Main1.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Main1 {
     public static void main(String[] args) {
-        int i = 2;
         double sum = 0;
-        do {sum += i;
-            ++i;}
-        while ( i <= 9);
-        System.out.println("Average: " + sum/7 );
+        int countEvenNumbers = 0;
+        int count = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите первое число диапазона: ");
+        int c  = sc.nextInt();
+        System.out.println("Введите последнее число диапазона: ");
+        int d =sc.nextInt();
+        do {sum += c;
+                ++c; count ++;}
+            while ( c <= d);
+
+        System.out.println("Среднее арифметическое: " + sum/ count);
+
+        System.out.println("Введите первое число диапазона: ");
+        int a = sc.nextInt();
+        System.out.println("Введите последнее число диапазона: ");
+        int b = sc.nextInt();
+        while (a <= b) {
+            if (a % 2 == 0) {
+                sum += a;
+                ++a;
+                count++;
+            }
+        }
+        System.out.println("Среднее арифметическое четных чисел: " + sum / countEvenNumbers);
+
     }
 }
