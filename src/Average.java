9ca9eca7f045ed434e066c16b122ea4142f25b2d
Maria1055 <96591820+Maria1055@users.noreply.github.com>
2022-03-14 23:47:06 +0700

    git commit

--- src/Average.java
+++ src/Average.java
@@ -3,21 +3,24 @@ import java.util.Scanner;
 public class Average {
     public static void main(String[] args) {
         int sum = 0;
-        int sumEvenNumbers =0;
+        int sumEvenNumbers = 0;
         int countEvenNumbers = 0;
         int count = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите первое число диапазона: ");
-        int c  = sc.nextInt();
+        int c = sc.nextInt();
         System.out.println("Введите последнее число диапазона: ");
-        int d =sc.nextInt();
-        for (c =1; c<=d; c++)
-        {sum += c;
-                ++c; count ++;
-        if (c % 2== 0) {sumEvenNumbers = sumEvenNumbers+c;
-            countEvenNumbers ++;}}
+        int d = sc.nextInt();
+        for (c = 1; c <= d; c++) {
+            sum += c;
+            count++;
+            if (c % 2 == 0) {
+                sumEvenNumbers = sumEvenNumbers + c;
+                countEvenNumbers++;
+            }
+        }
 
-        System.out.println("Среднее арифметическое: " + sum/ count);
+        System.out.println("Среднее арифметическое: " + (double) sum / count);
 
 
         System.out.println("Среднее арифметическое четных чисел: " + sumEvenNumbers / countEvenNumbers);
